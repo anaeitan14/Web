f38fd98f7b08961c1118f73a26dbda42f9bfe5ec
Eitan <eitanlich@yahoo.com>
2022-05-11 11:27:45 +0300

    11/05/2022

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ let title = document.getElementById('title');
 let author = document.getElementById('author');
 let category = document.getElementById('category');
 let searchBox = document.getElementById('search');
-let newBook = document.getElementById('addbook');
+let addBook = document.getElementById('addbook');
 let count = document.getElementById('count');
 let bookResult = document.getElementById('found');
 let search = document.getElementById('searchButton');
 
 
-search.addEventListener("click", searchBook);
-newBook.addEventListener("click", addBook);
-let books = [];
+search.addEventListener("click", Search);
+addBook.addEventListener("click", newBook);
+let library = [];
 
 function Book(title, author, category) {
     this.title=title.value;
@@ -19,24 +19,33 @@ function Book(title, author, category) {
 }
 
 
-function addBook() {
-    let newBook = new Book(title, author, category);
-    books.push(newBook);
-    count.innerHTML=books.length;
+function newBook() {
+    if(!(title.value == "" || author.value == "" || category.value == "")) {
+        let addBook = new Book(title, author, category);
+        library.push(addBook);
+        count.innerHTML=library.length + " Books";  
+    }
 }
 
-function searchBook() {
-    for(let i=0;i<books.length;i++){
-        if(searchBox.value == books[i].name || searchBox.value == books[i].author||
-            searchBox.value == books[i]) {
-                bookResult.innerHTML=this.name + this.author + this.category;
+function Search() {
+    bookResult.innerHTML="";
+    let userSearch = searchBox.value.toLowerCase();
+
+    for(let i=0;i<library.length;i++){
+        let bookTitle = library[i].title.toLowerCase();
+        let bookAuthor = library[i].author.toLowerCase();
+        let bookCategory = library[i].category.toLowerCase();
+
+        if (bookTitle.includes(userSearch) || bookAuthor.includes(userSearch)||
+            bookCategory.includes(userSearch)) {
+                bookResult.innerHTML+="Title: " +bookTitle+", Author: " + bookAuthor +", Category: " + bookCategory;
         }
     }
 }
 
 
 
-console.log(books);
+console.log(library);
 
 
 
